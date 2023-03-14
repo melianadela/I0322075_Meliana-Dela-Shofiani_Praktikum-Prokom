6eb2c14b2a240bd8e09e010a381edcb3d4dd2afb
Meliana Dela Shofiani <127015429+melianadela@users.noreply.github.com>
2023-03-14 15:34:11 +0700

    Add files via upload

---
+++ Exercise-01.py
@@ -0,0 +1,13 @@
+# Exercise-01
+print("====================")
+Nama ="Meliana Dela Shofiani"
+NIM ="I0322075"
+Asal ="Saya tinggal di Kabupaten Sragen, lebih tepatnya Kecamatan Sambirejo."
+Visualisasi ="Di sekitar kampung halaman saya masih terjaga keadaan alamnya. Jarak dari satu rumah ke rumah yang lainnya cukup memadai sehingga jauh dari kesan padat penduduk. Di sekitar rumah juga masih banyak kebun dan pepohonan serta kondisi udara yang masih segar."
+Harapan ="Harapan saya sebagai lulusan Teknik Industri yaitu dapat menerapkan apa yang saya pelajari dengan baik serta dapat hidup bahagia dan sukses dunia akhirat."
+print("Nama : ", Nama)
+print("NIM  : ", NIM)
+print(Asal)
+print(Visualisasi)
+print(Harapan)
+print("====================")
